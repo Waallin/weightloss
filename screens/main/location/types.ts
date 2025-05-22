1ffea3eadb0e2689018485ca61b817fbed6380a2
Viktor Wallin <95019086+Waallin@users.noreply.github.com>
2025-05-22 14:09:38 +0200

    implement location view frontend

---
+++ screens/main/location/types.ts
@@ -0,0 +1,15 @@
+export type HistoryItemType = {
+  id: number;
+  location: string;
+  coordinates: string;
+  status: string;
+  time: string;
+};
+
+export type NearbyItemType = {
+  id: number;
+  name: string;
+  coordinates: string;
+  distance: string;
+  type: string;
+};
