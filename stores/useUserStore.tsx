b27460cd1396bbfa8bb6e09f13cd387a401d2ff1
Viktor Wallin <95019086+Waallin@users.noreply.github.com>
2025-06-16 08:28:06 +0200

    Implement api to preferences

--- stores/useUserStore.tsx
+++ stores/useUserStore.tsx
@@ -1,17 +1,12 @@
 import { create } from "zustand";
 
-const dummyUser = {
-  id: "1",
-  name: "Morgana",
-  token: "1|fxRaW14xHkA7hvbdNG6dsC3pjzWHWgxRDkqvF5oxfdb514f9",
-};
 interface UserState {
   user: any;
   setUser: (user: any) => void;
 }
 
 const useUserStore = create<UserState>((set, get) => ({
-  user: dummyUser,
+  user: null,
   setUser: (user) => set({ user }),
 }));
 
